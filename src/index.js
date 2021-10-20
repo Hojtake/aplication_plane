c0f52024c0b46fb7c480bc2ec436262983280a78
Hojtake <89243337+Hojtake@users.noreply.github.com>
2021-10-20 10:59:21 +0900

    2021_10_20 commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./styles.css";
+
+const onclickAdd = () => {
+  const inputtext = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = inputtext; //テキストの追加を行うコマンド
+
+  const completeButten = document.createElement("button");
+  completeButten.innerText = "完了";
+
+  const deleteButten = document.createElement("button");
+  completeButten.innerText = "削除";
+
+  div.appendChild(li);
+  div.appendChild(completeButten);
+  div.appendChild(deleteButten);
+  console.log(div);
+  document.getElementById("imcomplete_list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onclickAdd());
